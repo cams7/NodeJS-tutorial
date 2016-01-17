3404ddc51a0bce5a0d74d4b310b71d76f49110e3
cams7-qbex <ceanma@gmail.com>
2016-01-17 14:52:20 -0200

    Perguntas mais frequentes, testado do site: http://expressjs.com

--- express/introducao/myapp/app.js
+++ express/introducao/myapp/app.js
@@ -22,6 +22,15 @@ app.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user');
 });
 
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
